4d615b79fa8012d8a9983c1bd2d3d8740a0093d8
SenorGrande <clooneyjh@gmail.com>
2024-05-13 19:47:19 +1200

    refactor: remove uneeded dependency

--- src/flight/flight.service.ts
+++ src/flight/flight.service.ts
@@ -1,10 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EventEmitter2 } from '@nestjs/event-emitter';
-import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CronJob } from 'cron';
-import { Observable } from 'rxjs';
 import { EntityManager, Repository } from 'typeorm';
 import { AirlineDto } from './dto';
 import { FlightDto } from './dto/flight.dto';
@@ -28,7 +26,6 @@ export class FlightService {
     @InjectRepository(Flight)
     private readonly flightRepository: Repository<Flight>,
     private readonly entityManager: EntityManager,
-    private schedulerRegistry: SchedulerRegistry, // IN MEMORY, NOT DISTRIBUTED IF MULTIPLE PODS RUNNING
     private eventEmitter: EventEmitter2,
   ) {
     // const flightCron = schedulerRegistry.getCronJob('generate_days_flights');
@@ -78,7 +75,10 @@ export class FlightService {
 
   async getSchedule() { }
 
-  async getAllSchedules() { }
+  async getAllSchedules(): Promise<FlightSchedule[]> {
+    const schedules = await this.flightScheduleRepository.find();
+    return schedules;
+  }
 
   async createSchedule(dto: ScheduleDto) {
     const newSchedule = new FlightSchedule({
@@ -180,104 +180,102 @@ export class FlightService {
 
   // EVERY_DAY_AT_MIDNIGHT for actual
   // EVERY_10_SECONDS for testing
-  // @Cron(CronExpression.EVERY_10_SECONDS, { name: 'generate_days_flights' })
   // TODO : create AWS SNS topic on flight creation - would need another cron to delete old topics
+  @Cron(CronExpression.EVERY_10_SECONDS, { name: 'generate_days_flights', disabled: process.env.ENABLE_FLIGHT_CRON == "false" })
   async generateDaysFlights(name: string) {
 
-    const job = new CronJob(CronExpression.EVERY_10_SECONDS, async () => {
+    this.logger.log('Generating this days flights...');
 
-      this.logger.log('Generating this days flights...');
+    const date = new Date();
+    date.setDate(date.getDate() + 1); // Shift it to tomorrow
+    const d = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate()
+    );
 
-      const date = new Date();
-      date.setDate(date.getDate() + 1); // Shift it to tomorrow
-      const d = new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate()
-      );
-
-      // Get latest date in flights table
-      const latestFlight = await this.flightRepository.findOne({
-        select: { flightDate: true },
-        where: {},
-        order: { flightDate: "DESC" }
-      });
-
-      // To make sure there is no time from the DB timestamp
-      let latestFlightDate = null;
-      if (latestFlight != null) {
-        latestFlightDate = new Date(
-          latestFlight.flightDate.getFullYear(),
-          latestFlight.flightDate.getMonth(),
-          latestFlight.flightDate.getDate()
-        );
-
-        this.logger.log("===> Latest record");
-        this.logger.log(latestFlight.flightDate);
-        this.logger.log(d);
-        this.logger.log(latestFlightDate);
-      }
-
-      // works if latestFlightDate is null
-      if (!(latestFlightDate < d)) {
-        this.logger.log("Flights are up to date?");
-        return;
-      } else {
-        this.logger.log("Get new flights");
-      }
-
-      // New flights need to be generated and added to the table
-      // Get current day of the week
-      const day = d.getDay();
-      const weekday = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-      const dayObj = {};
-      dayObj[weekday[day]] = true;
-      this.logger.log(`Today is ${weekday[day]}`);
-
-      const flightList = await this.flightScheduleRepository.find({
-        where: dayObj,
-        relations: { airline: true },
-        order: { scheduledDepartureTime: "ASC" }
-      });
-
-      this.logger.log("List of flights");
-      this.logger.log(flightList);
-
-      // Status - get the ontime status
-      let status: Status = await this.statusRepository.findOne({
-        where: {
-          name: "On Time"
-        }
-      });
-
-      // Insert into flights - loop through
-      let flights: Flight[] = [];
-      flightList.forEach((flightSchedule: FlightSchedule) => { // ! removed , i
-        let flightObj = new FlightDto();
-        flightObj.flightSchedule = flightSchedule;
-        flightObj.estimatedDepartureTime = flightSchedule.scheduledDepartureTime;
-        flightObj.flightDate = d; // TODO : not sure if this is best, do we want time?
-        flightObj.status = status;
-
-        this.logger.log("Flight Obj");
-        this.logger.log(flightObj);
-
-        const flightEntity = new Flight({
-          ...flightObj
-        });
-
-        flights.push(flightEntity);
-
-      });
-
-      this.logger.log("Flights");
-      this.logger.log(flights);
-
-      await this.entityManager.save(flights);
+    // Get latest date in flights table
+    const latestFlight = await this.flightRepository.findOne({
+      select: { flightDate: true },
+      where: {},
+      order: { flightDate: "DESC" }
     });
 
-    this.schedulerRegistry.addCronJob(name, job);
-    job.start();
+    // To make sure there is no time from the DB timestamp
+    let latestFlightDate = null;
+    if (latestFlight != null) {
+      latestFlightDate = new Date(
+        latestFlight.flightDate.getFullYear(),
+        latestFlight.flightDate.getMonth(),
+        latestFlight.flightDate.getDate()
+      );
+
+      this.logger.log("===> Latest record");
+      this.logger.log(latestFlight.flightDate);
+      this.logger.log(d);
+      this.logger.log(latestFlightDate);
+    }
+
+    // works if latestFlightDate is null
+    if (!(latestFlightDate < d)) {
+      this.logger.log("Flights are up to date?");
+      return;
+    } else {
+      this.logger.log("Get new flights");
+    }
+
+    // New flights need to be generated and added to the table
+    // Get current day of the week
+    const day = d.getDay();
+    const weekday = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    const dayObj = {};
+    dayObj[weekday[day]] = true;
+    this.logger.log(`Today is ${weekday[day]}`);
+
+    const flightList = await this.flightScheduleRepository.find({
+      where: dayObj,
+      relations: { airline: true },
+      order: { scheduledDepartureTime: "ASC" }
+    });
+
+    this.logger.log("List of flights");
+    this.logger.log(flightList);
+
+    // Status - get the ontime status
+    let status: Status = await this.statusRepository.findOne({
+      where: {
+        name: "On Time"
+      }
+    });
+
+    // Insert into flights - loop through
+    let flights: Flight[] = [];
+    flightList.forEach((flightSchedule: FlightSchedule) => { // ! removed , i
+      let flightObj = new FlightDto();
+      flightObj.flightSchedule = flightSchedule;
+      flightObj.estimatedDepartureTime = flightSchedule.scheduledDepartureTime;
+      flightObj.flightDate = d; // TODO : not sure if this is best, do we want time?
+      flightObj.status = status;
+
+      this.logger.log("Flight Obj");
+      this.logger.log(flightObj);
+
+      const flightEntity = new Flight({
+        ...flightObj
+      });
+
+      flights.push(flightEntity);
+
+    });
+
+    this.logger.log("Flights");
+    this.logger.log(flights);
+
+    await this.entityManager.save(flights);
+
+
+    // this.schedulerRegistry.addCronJob(name, job);
+    // job.start();
 
     this.logger.warn(
       `job ${name} added for every ${CronExpression.EVERY_10_SECONDS}!`,
